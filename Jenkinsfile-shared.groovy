ecd6160eb745192be3236b7fec6733349d66054e
krash-palo <krash@paloaltonetworks.com>
2025-07-24 10:39:14 -0400

    Update Jenkinsfile-shared.groovy

--- Jenkinsfile-shared.groovy
+++ Jenkinsfile-shared.groovy
@@ -14,7 +14,7 @@ def execute() {
                     --use-enforcement-rules \
                     -o cli -o junitxml \
                     --output-file-path console,results.xml \
-                    --bc-api-key ${pc_user}::${pc_password} \
+                    --bc-api-key cc67ac84-1d07-4643-95e6-7b24780fa0c1::WuRn23bdxr8YlICIfk6OQewEE40= \
                     --repo-id krash/terragoat \
                     --branch master
                 '''
